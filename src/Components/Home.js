ff1efc2bb20c6a8f6e52103f1f199d30a30ed8ff
alfredo-saenz <alfredosaenz104@gmail.com>
2022-10-10 13:25:09 -0500

    Made the skeleton of the layout

---
+++ src/Components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Home(props) {
+    return (
+        <div>
+            <h1>Welcome</h1>
+            <p>Hello, I'm Alfredo Saenz the UI/Ux designer</p>
+        </div>
+    );
+}
+
+export default Home;
---
+++ src/Components/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Navbar(props) {
+    return (
+        <div>
+            <ul>
+                <li>Alfredo Saenz</li>
+                <li>About</li>
+                <li>Projects</li>
+                <li>Contact</li>
+            </ul>
+        </div>
+    );
+}
+
+export default Navbar;
