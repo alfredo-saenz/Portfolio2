f3d28f693f6024c83f945e1b79037621824e37de
alfredo-saenz <alfredosaenz104@gmail.com>
2022-10-11 15:52:03 -0500

    Made the nav bar with responsiveness

--- src/App.js
+++ src/App.js
@@ -2,15 +2,20 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from './Components/Navbar';
 import Home from './Components/Home';
+import Contact from './Components/Contact';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <>
       <Navbar/>
-      <Home/>
+      {/* <Home/> */}
 
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/contact' element={<Contact/>}/>
+      </Routes>
     </>
-    
   );
 }
 
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,16 +1,31 @@
 import React from 'react';
+import {Link} from 'react-router-dom'
+import {FaBars, FaTimes} from 'react-icons/fa'
+import {useRef} from 'react'
+import './Navbar.css'
 
 function Navbar(props) {
+    const navRef = useRef();
+
+    const showNavbar = () => {
+        navRef.current.classList.toggle('responsive_nav');
+    }
     return (
-        <div>
-            <ul>
-                <li>Alfredo Saenz</li>
-                <li>About</li>
-                <li>Projects</li>
-                <li>Contact</li>
-            </ul>
-        </div>
-    );
+			<header>
+                <h4>Alfredo Saenz</h4>
+				<nav ref={navRef}>
+					<a href='/#'>About me</a>
+					<a href='/#'>Projects</a>
+					<a href='/#'>Contact</a>
+					<button className='nav-btn nav-close-btn' onClick={showNavbar}>
+						<FaTimes />
+					</button>
+				</nav>
+				<button className='nav-btn' onClick={showNavbar}>
+					<FaBars />
+				</button>
+			</header>
+		);
 }
 
 export default Navbar;
